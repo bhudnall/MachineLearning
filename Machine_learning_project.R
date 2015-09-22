a084c3820cce2dc53ced5289b657868d7fa30171
bhudnall <brian.hudnall@gmail.com>
2015-09-21 21:33:15 -0700

    Updated Process

--- Machine_learning_project.R
+++ Machine_learning_project.R
@@ -11,17 +11,22 @@
 
 ## Goals of the project
 #     - Predict the manner in which someone did the exercise ("classe") variable in data set
-#     - Can use any variable in the data set to ppredict
+#     - Can use any variable in the data set to predict
 #     - Describe how:
-#         - built model - USE REGRESSION POTENTIALLY
+#         - built moded - regression (REVIEW REGRESSION VIDEO AGAIN), decision tree (use the 
+#           rpart method in the train function), Random Forest (use method="rf" with the train
+#           function. Use fancyRPartPlot to create a good looking dendogram. Predict new values 
+#           using the predict function.
 #         
 #         - cross validation - Essentially the practice of splitting up the data set, using a 
 #            training set to build the model and use it on the test set, methods: K-fold? 
-#            leave one out? 
+#            leave one out? Use the train function to do cross validation.
 #         
 #         - what expected out of sample error is - The error rate you get on a new data set.
 #           Sometimes called generalization error. So the error I'll get in the test set.
-#           In sample error is the error we get from the training set. 
+#           In sample error is the error we get from the training set. How to calculate:
+#           Look at type of errors slides. Can caluclated it with mean squared error for
+#           continuous data, or weight false positives
 #         
 #         - why made choices did
 #     - Use prediction model to precist 20 test cases (testing set)
@@ -33,14 +38,23 @@
 
 ## Plan
 #     - First explore data set, see what variables are included.
+#     
 #     - Split the data using createDataPartition, use p=0.75
+#     
 #     - Do some preprocessing using the preProcess function. This is to figure out
 #       what predictor variables to use in the model. This will also allow you to
 #       center and scale variables that have large variability. Might have to imput 
 #       (method="knnImput") values that are missing.
-#     - Create an algorithm using the train function, test different algorithms using train 
+#     
+#     - Create an algorithm using the train function (Decision Tree or random forest)
+#       Rnadom forest is essentially decision tree with bagging.
+#
+#     - Test different algorithms using train and bagging (non-linear models). Potentially
+#       boost to enhance week variables.
+#     
 #     - ConfusionMatrix - take the output from the predict function and pass
 #       it to the ConfusionMatrix to see the accuracy
+#     
 #     - test the algorithm on the test set using the predict function
 
 
